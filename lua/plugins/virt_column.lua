aa948504ae92db498cad1c5d3178fe50ff9397d3
hopezh <hope.zh@gmail.com>
2024-02-02 13:09:43 +0800

    disable virt-column

--- lua/plugins/virt_column.lua
+++ lua/plugins/virt_column.lua
@@ -1,12 +1,13 @@
 return {
     "lukas-reineke/virt-column.nvim",
-
+    enabled = false,
     ----------------------------------------------------------------------------
     config = function()
         require("virt-column").setup({
+            char = ".",
             -- char = "║",
             -- char = "|",
-            char = "│",
+            -- char = "│",
             -- char = "▏",
         })
     end,
