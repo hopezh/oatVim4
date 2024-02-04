65be767e1bc6ca8d2a33d0b21e7eae8f201d0e88
hopezh <hope.zh@gmail.com>
2024-02-04 23:29:03 +0800

    add outline.lua

---
+++ lua/plugins/outliine.lua
@@ -0,0 +1,32 @@
+return {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+
+    keys = { -- Example mapping to toggle outline
+        {
+            "<leader>co",
+            "<cmd>Outline<CR>",
+            desc = "Toggle outline",
+        },
+    },
+
+    opts = {
+        -- Your setup opts here
+    },
+
+    -- -- stylua: ignore
+    -- config = function()
+    --     -- Example mapping to toggle outline
+    --     vim.keymap.set(
+    --         "n",
+    --         "<leader>o",
+    --         "<cmd>Outline<CR>",
+    --         { desc = "Toggle Outline" }
+    --     )
+    --
+    --     require("outline").setup({
+    --         -- Your setup opts here (leave empty to use defaults)
+    --     })
+    -- end,
+}
